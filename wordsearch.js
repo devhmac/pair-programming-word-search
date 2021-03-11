61c10bdec0bcdb72707621e23dcb15d8a08a710a
Devin MacGillivray <devhmac@gmail.com>
2021-03-11 00:52:02 +0000

    pair program session submission with katrina Song

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+//Pair programmed: Devin MacGillivray and Karina Song
+
+const wordSearch = (letters, word) => {
+
+    if (letters.length === 0) {
+        return false;
     }
+    //this looks for the word horizontally
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    //console.log(horizontalJoin);
+    for (let l of horizontalJoin) {
+        //console.log(l)
+        if (l.includes(word)) {
+            //console.log(l.includes(word))
+            return true;
+        }
+    }
+    // Checks for word vertically
+    const verticalArr = []
+    let vertRow = ''
+    for (let i = 0; i < letters.length; i++) {
+        vertRow = '';
+        for (let j = 0; j < letters[i].length + 1; j++) {
+            if (i < letters.length - 1) {
+                vertRow += letters[j][i];
+                //console.log(vertRow)
+            }
+        }
+        verticalArr.push(vertRow)
+    }
+    for (let colWord of verticalArr) {
+        if (colWord.includes(word)) return true
+    }
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
